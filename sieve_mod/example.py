da9bf0c72aa9537a75e8d70f5ecd6295cd9528ea
Michelle Vogel <vogelmi3@msu.edu>
2013-01-17 23:11:47 -0500

    added sieve_mod

---
+++ sieve_mod/example.py
@@ -0,0 +1,4 @@
+import sieve
+
+for n in range(10):
+    print sieve.next()
---
+++ sieve_mod/sieve.py
@@ -0,0 +1,17 @@
+_primeslist = [2]
+
+def _is_prime(primes, n):
+    for i in primes:
+        if n % i == 0:
+            return False
+    return True
+
+def next():
+    start = _primeslist[-1] + 1
+    while 1:
+        if _is_prime(_primeslist, start):
+            _primeslist.append(start)
+            return start
+        start += 1
+
+
